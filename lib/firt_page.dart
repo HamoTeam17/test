ed76811824c506671c059ec899919e283e135618
hamo <hamoteam17@gmail.com>
2022-12-26 15:25:05 +0200

    first commit

---
+++ lib/firt_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+class FirstPage extends StatefulWidget {
+  const FirstPage({super.key});
+
+  @override
+  State<FirstPage> createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Chip(label: Text('First Page'),
+        backgroundColor: Colors.grey,
+        
+        ),
+      ),
+    );
+  }
+}
